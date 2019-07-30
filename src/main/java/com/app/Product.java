37791cc0f410ada02dc82397e60a26a5d8ff60e0
vipinjava89@gmail.com <vipinjava89@gmail.com>
2019-07-30 23:11:32 +0530

    This is to demonstrate log4j (Tool)

---
+++ src/main/java/com/app/Product.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import org.apache.log4j.Logger;
+
+public class Product {
+private static Logger log = Logger.getLogger(Product.class);
+public static void main(String[] args) {
+	log.debug("HELLO");
+	log.info("HELLO");
+	log.warn("HELLO");
+	log.error("HELLO");
+	log.fatal("HELLO");
+	
+}
+}
+
+
+
+
+
